ac81dfa295f5bfa03d6ed109ab388e395f93a4b9
nikst <e>
2017-11-06 13:54:55 +0100

    design

--- FERIFox/MainWindow.xaml.cs
+++ FERIFox/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace FERIFox
         public MainWindow()
         {
             InitializeComponent();
+            Browser.Navigate(new Uri("http://www.google.si"));
+            
         }
     }
 }
