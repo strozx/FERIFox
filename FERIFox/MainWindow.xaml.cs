6cff9ea1ef2f8baf9c306778d1a0518c097e393d
nikst <niik.strozak@outlook.com>
2018-01-16 20:06:27 +0100

    JHKC

--- FERIFox/MainWindow.xaml.cs
+++ FERIFox/MainWindow.xaml.cs
@@ -22,9 +22,162 @@ namespace FERIFox
     {
         public MainWindow()
         {
+        
             InitializeComponent();
-            Browser.Navigate(new Uri("http://www.google.si"));
-            
+            wbSample.Navigate("http://www.google.si");
+            // initialize tabItem array
+            _tabItems = new List<TabItem>();
+
+            // add a tabItem with + in header 
+            TabItem tabAdd = new TabItem();
+            tabAdd.Header = "+";
+
+            _tabItems.Add(tabAdd);
+
+            // add first tab
+            this.AddTabItem();
+
+            // bind tab control
+            tabDynamic.DataContext = _tabItems;
+
+            tabDynamic.SelectedIndex = 0;
+        }
+        private TabItem AddTabItem()
+        {
+            int count = _tabItems.Count;
+
+            // create new tab item
+            TabItem tab = new TabItem();
+            tab.Header = string.Format("Tab {0}", count);
+            tab.Name = string.Format("tab{0}", count);
+            tab.HeaderTemplate = tabDynamic.FindResource("TabHeader") as DataTemplate;
+
+            // add controls to tab item, this case I added just a textbox
+            TextBox txt = new TextBox();
+            txt.Name = "txt";
+            tab.Content = txt;
+
+            // insert tab item right before the last (+) tab item
+            _tabItems.Insert(count - 1, tab);
+            return tab;
+        }
+        private List<TabItem> _tabItems;
+        private TabItem _tabAdd;
+        private void txtUrl_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                wbSample.Navigate(txtUrl.Text);
+        }
+
+        private void wbSample_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            txtUrl.Text = e.Uri.OriginalString;
+        }
+
+        private void BrowseBack_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((wbSample != null) && (wbSample.CanGoBack));
+        }
+
+        private void BrowseBack_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.GoBack();
+        }
+
+        private void BrowseForward_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((wbSample != null) && (wbSample.CanGoForward));
+        }
+
+        private void BrowseForward_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.GoForward();
+        }
+
+        private void GoToPage_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void GoToPage_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.Navigate(txtUrl.Text);
+        }
+
+        private void Refresh_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.Refresh();
+        }
+
+        private void Refresh_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void tabDynamic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabItem tab = tabDynamic.SelectedItem as TabItem;
+
+            if (tab != null && tab.Header != null)
+            {
+                if (tab.Header.Equals(_addTabHeader))
+                {
+                    // clear tab control binding
+                    tabDynamic.DataContext = null;
+
+                    // add new tab
+                    TabItem newTab = this.AddTabItem();
+
+                    // bind tab control
+                    tabDynamic.DataContext = _tabItems;
+
+                    // select newly added tab item
+                    tabDynamic.SelectedItem = newTab;
+                }
+                else
+                {
+                    // your code here...
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+            string tabName = (sender as Button).CommandParameter.ToString();
+
+            var item = tabDynamic.Items.Cast<tabitem>().Where(i => i.Name.Equals(tabName)).SingleOrDefault();
+
+            TabItem tab = item as TabItem;
+
+            if (tab != null)
+            {
+                if (_tabItems.Count < 3)
+                {
+                    MessageBox.Show("Cannot remove last tab.");
+                }
+                else if (MessageBox.Show(string.Format("Are you sure you want to remove the tab '{0}'?", tab.Header.ToString()),
+                    "Remove Tab", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    // get selected tab
+                    TabItem selectedTab = tabDynamic.SelectedItem as TabItem;
+
+                    // clear tab control binding
+                    tabDynamic.DataContext = null;
+
+                    _tabItems.Remove(tab);
+
+                    // bind tab control
+                    tabDynamic.DataContext = _tabItems;
+
+                    // select previously selected tab. if that is removed then select first tab
+                    if (selectedTab == null || selectedTab.Equals(tab))
+                    {
+                        selectedTab = _tabItems[0];
+                    }
+                    tabDynamic.SelectedItem = selectedTab;
+                }
+            }
         }
     }
 }
